a73c5a95ba1764bd5998d5467e7dcd54f2597661
dor917 <70497341+dor917@users.noreply.github.com>
2021-07-25 23:22:07 +0900

    buffer 공부 시작

---
+++ src/com/kdh/step1to10/no15552.java
@@ -0,0 +1,22 @@
+package com.kdh.step1to10;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class no15552 {
+	
+	public static void main(String[] args) {
+		
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			// TODO: handle finally clause
+		}
+	
+
+	}
+}
